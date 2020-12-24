2f63f9e8375dddcdea9998c87269ab9d28c90cfb
JIn Kimura <jin@JInnoMacBook-Pro.local>
2020-12-24 20:05:15 +0900

    amend1224

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -1,14 +1,8 @@
 <?php
+ ini_set('error_reporting', E_ALL);
 
- $fizznum_str = $_POST["FizzNum"];
- $buzznum_str = $_POST["BuzzNum"];
-
- $fizznum = (int)$fizznum_str;
- $buzznum = (int)$buzznum_str;
-
- $fizznum_float = (float)$fizznum_str;
- $buzznum_float = (float)$buzznum_str;
-
+ $fizznum = mb_convert_kana($_POST["FizzNum"],"n");
+ $buzznum = mb_convert_kana($_POST["BuzzNum"],"n");
 
 ?>
 <!DOCTYPE html>
@@ -18,33 +12,39 @@
       <title>FizzBuzz問題</title>
 </head>
 <body>
-  <form class="" action="#" method="post">
+  <form method="post">
     <p>
-      FizzNum:　<input type="text" name="FizzNum" value="整数値を入力してください" size="30">
+      FizzNum:　<input type="text" name="FizzNum" placeholder="整数値を入力してください" size="30">
     </p>
     <p>
-      BuzzNum: <input type="text" name="BuzzNum" value="整数値を入力してください" size="30">
+      BuzzNum: <input type="text" name="BuzzNum" placeholder="整数値を入力してください" size="30">
     </p>
     <p>
-      <input type="submit" name="" value="実行">
+      <input type="submit" name="submit" value="実行">
     </p>
   </form>
   <p>【出力】</p>
-  <?php if($fizznum == 0 || $buzznum ==0 ): ?>
-    <p><?php echo "整数値を入力してください" ?></p>
-  <?php elseif($fizznum != $fizznum_float || $buzznum != $buzznum_float): ?>
-    <p><?php echo "整数値を入力してください" ?></p>
-  <?php else: ?>
-    <?php for($i=1;$i<100;$i++): ?>
-      <?php if($i%$fizznum == 0 && $i%($fizznum*$buzznum) !=0 ): ?>
-        <p><?php echo "Fizz ".$i; ?></p>
-      <?php elseif($i%$buzznum == 0 && $i%($fizznum*$buzznum) !=0 ):?>
-        <p><?php echo "Buzz ".$i; ?></p>
-      <?php elseif($i%($fizznum*$buzznum) ==0 ): ?>
-        <p><?php echo "FizzBuzz ".$i; ?></p>
-      <?php endif; ?>
-    <?php endfor; ?>
-  <?php endif; ?>
+  <?php
+  if($_POST["submit"] === "実行"){
+    if((int)$fizznum === 0 || (int)$buzznum === 0){
+      echo "<p>整数値を入力してください</p>";
+    }elseif((int)$fizznum != (float)$fizznum || (int)$buzznum != (float)$buzznum ){
+      echo "<p>整数値を入力してください</p>";
+    }else{
+      for($i=1;$i<100;$i++){
+        if($i%$fizznum == 0 && $i%($fizznum*$buzznum) !=0 ){
+          echo "<p>Fizz ".$i."</p>";
+        }elseif($i%$buzznum == 0 && $i%($fizznum*$buzznum) !=0 ){
+          echo "<p>Buzz ".$i."</p>";
+        }elseif($i%($fizznum*$buzznum) ==0 ){
+          echo "<p>FizzBuzz ".$i."</p>";
+        }
+      }
+    }
+  }
+
+   ?>
+
 </body>
 
 </html>
