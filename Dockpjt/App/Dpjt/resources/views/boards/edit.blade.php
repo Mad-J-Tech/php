74cb88652ab6c9ba1c8b32b6f0ea0da7ec276cdd
JIn Kimura <jellyfishman520@gmail.com>
2021-04-06 06:50:42 +0900

    [Update]掲示板　レイアウトの追加　indexページデザイン

--- Dockpjt/App/Dpjt/resources/views/boards/edit.blade.php
+++ Dockpjt/App/Dpjt/resources/views/boards/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.boards')
 
 @section('content')
 <form action="/boards/{{$item->id}}" method="POST">
--- Dockpjt/App/Dpjt/resources/views/boards/index.blade.php
+++ Dockpjt/App/Dpjt/resources/views/boards/index.blade.php
@@ -1,21 +1,34 @@
-@extends('layouts.app')
+@extends('layouts.boards')
 
 @section('content')
 @foreach($items as $item)
-<h1>{{$item->title}}</h1>
-<h2>{{$item->message}}</h2>
-<h3>{{$item->getName()}}</h3>
-@if($item->user_id === $myid)
-<form action="/boards/{{$item->id}}/edit" method="GET">
-    @csrf
-    <button type="submit">編集</button>
-</form>
-<form action="/boards/{{$item->id}}" method="POST">
-    @method('delete')
-    @csrf
-    <button type="submit">削除</button>
-</form>
-@endif
+<div class="card text-center">
+    <div class="card-body">
+        <h2 class="card-title">{{$item->title}}</h2>
+        <p class="card-text">{{$item->message}}</p>
+        <h3>{{$item->getName()}}</h3>
+        <div class="container">
+            <div class="row">
+                @if($item->user_id === $myid)
+                <div class="col">
+                    <form action="/boards/{{$item->id}}/edit" method="GET">
+                        @csrf
+                        <button class="btn btn-primary" type="submit">編集</button>
+                    </form>
+                </div>
+                <div class="col">
+                    <form action="/boards/{{$item->id}}" method="POST">
+                        @method('delete')
+                        @csrf
+                        <button class="btn btn-danger" type="submit">削除</button>
+                    </form>
+                </div>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
 @endforeach
+
 
 @endsection
